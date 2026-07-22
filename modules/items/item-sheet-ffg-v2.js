5db5e56ae86a06b4667886012dbc79b955c7b059
Gebri <gabrielroberto1111@gmail.com>
2026-07-22 15:09:43 -0700

    Item Description Open by Default

--- modules/items/item-sheet-ffg-v2.js
+++ modules/items/item-sheet-ffg-v2.js
@@ -5,7 +5,7 @@ export class ItemSheetFFGV2 extends ItemSheetFFG {
   static get defaultOptions() {
     return foundry.utils.mergeObject(super.defaultOptions, {
       classes: ["starwarsffg", "sheet", "item", "v2"],
-      tabs: [{ navSelector: ".sheet-tabs", contentSelector: ".sheet-body", initial: "attributes" }],
+      tabs: [{ navSelector: ".sheet-tabs", contentSelector: ".sheet-body", initial: "description" }],
       scrollY: [".sheet-body", ".tab"],
     });
   }
